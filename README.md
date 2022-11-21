4217f4e5707f88bb55c974492a06e6bd28cf9c12
Eli Richardson <elijahrich02@icloud.com>
2022-11-20 21:43:20 -0600

    njs v2

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # njs.icu
 
-A clone of [emdn.me](https://github.com/eli-rich/emdn.me) except for NodeJS docs.
+Does your code need ICU treatment?
+
+Use `njs.icu` to quickly search docs for many popular frameworks and tools.
+(Feel free to PR with more libraries.)
+
+Example usage: https://njs.icu/next/getServerSideProps
--- main.go
+++ main.go
@@ -4,15 +4,16 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"regexp"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type Config struct {
 	Engine   string `json:"engine"`
-	Search   string `json:"search"`
 	Fallback string `json:"fallback"`
 	Port     string `json:"port"`
+	Paths    map[string]string
 }
 
 func main() {
@@ -27,12 +28,46 @@ func main() {
 	if err := json.Unmarshal(configFile, &config); err != nil {
 		log.Fatalln(err)
 	}
+	pathRegex := regexp.MustCompile(`\/(.*)\/`)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	app := fiber.New()
+	app.Get("/*/:query", func(c *fiber.Ctx) error {
+		results := pathRegex.FindStringSubmatch(c.Path())
+		if len(results) == 0 {
+			return c.Redirect(config.Fallback)
+		}
+		path := results[1]
+		search := config.Paths[path]
+		if search == "" {
+			return c.Redirect(config.Fallback)
+		}
+		return c.Redirect(config.Engine + c.Params("query") + " " + search)
+	})
+	app.Get("/*", func(c *fiber.Ctx) error {
+		results := pathRegex.FindStringSubmatch(c.Path())
+		if len(results) == 0 {
+			return c.Redirect(config.Fallback)
+		}
+		path := results[1]
+		search := config.Paths[path]
+		if search == "" {
+			return c.Redirect(config.Fallback)
+		}
+		return c.Redirect("https://" + search[5:])
+	})
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Redirect(config.Fallback)
 	})
-	app.Get("/:query", func(c *fiber.Ctx) error {
-		return c.Redirect(config.Engine + c.Params("query") + " " + config.Search)
-	})
+	go forwardToHttps()
 	app.ListenTLS(":"+config.Port, "cert.pem", "key.pem")
+}
+
+func forwardToHttps() {
+	app := fiber.New()
+	app.Get("/*", func(c *fiber.Ctx) error {
+		return c.Redirect("https://" + c.Hostname() + c.Path())
+	})
+	app.Listen(":80")
 }
